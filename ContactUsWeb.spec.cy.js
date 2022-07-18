19a1b4d454cb08621fe58a161e0e2617ef7d1cbf
Kasioautomat <109282307+Kasioautomat@users.noreply.github.com>
2022-07-18 11:16:16 +0200

    Add files via upload

---
+++ ContactUsWeb.spec.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('My first automated test for ITM', () => {
+
+
+  it('Verify reset', () => {
+    cy.visit('https://www.itmagination.com/contact')
+
+  })
+
+  
+});
+
+
+
+
+
